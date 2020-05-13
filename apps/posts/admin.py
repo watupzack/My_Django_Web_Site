20258ac5cfb3df9bef2d07ef82b60e8dfeff8ec4
Maxim Rezan <tvletsplay412@gmail.com>
2020-05-13 22:56:46 +0300

    some updates

--- apps/posts/admin.py
+++ apps/posts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "author", "created_at", "status"]
+    list_filter = ["title", "author", "created_at"]
+    search_fields = ["title", "text"]
+    actions = ["make_published"]
+
+    def make_published(self, request, queryset):
+        queryset.update(status='p')
+
+    make_published.short_description = "Mark selected post as published"
+
+
+admin.site.register(Post, PostAdmin)
--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+STATUS_CHOICES = [
+    ('d', 'Draft'),
+    ('p', 'Published'),
+    ('w', 'Withdrawn'),
+]
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=120)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0])
+
+    def __str__(self):
+        return self.title
